309844885a41e115df9241acd3b8a9679f6d88fd
Evgeniy <zadrod93@yandex.ru>
2022-03-05 16:25:52 +0300

    Задача номер 69

---
+++ Example069/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 15
+// M = 4; N = 8. -> 5
+
+void Numbers(int m, int n)
+{
+    if (m < n)
+    {
+        FindSum(m, n);
+    }
+
+}
+void FindSum(int m, int n)
+{
+    int sum = m;
+    while (m < n)
+    {
+        m++;
+        sum = sum + m;
+    }
+    Console.Write(sum);
+}
+
+Numbers(1, 15);
+Console.WriteLine();
+Numbers(4, 8);
